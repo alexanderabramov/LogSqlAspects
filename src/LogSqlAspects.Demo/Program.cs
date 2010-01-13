8e8f72a3d9f39e6fcc03ee200c3b785c9204433a
Alexander Abramov <alex.v.abramov@gmail.com>
2010-01-13 19:10:42 +0100

    initial version

---
+++ src/LogSqlAspects.Demo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using log4net.Config;
+
+// I'd expect one entry to cover IDbCommand, Common.DbCommand, derivatives and implementers,
+// but it does not do so
+//[assembly: LogSqlAspects.LogAdoNetAttribute(
+//    AttributeTargetAssemblies = "System.Data",
+//    AttributeTargetTypes = "System.Data.*DbCommand",
+//    AttributeTargetMembers = "Execute*" // ExecuteScalar, NonQuery, Reader
+//    )]
+
+[assembly: LogSqlAspects.LogAdoNetAttribute(
+    AttributeTargetAssemblies = "System.Data",
+    AttributeTargetTypes = "System.Data.IDbCommand",
+    AttributeTargetMembers = "Execute*"
+    )]
+//[assembly: LogSqlAspects.LogAdoNetAttribute(
+//    AttributeTargetAssemblies = "System.Data",
+//    AttributeTargetTypes = "System.Data.Common.DbCommand",
+//    AttributeTargetMembers = "Execute*"
+//    )]
+//[assembly: LogSqlAspects.LogAdoNetAttribute(
+//    AttributeTargetAssemblies = "System.Data",
+//    AttributeTargetTypes = "System.Data.SqlClient.SqlCommand",
+//    AttributeTargetMembers = "Execute*"
+//    )]
+
+namespace LogSqlAspects.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+
+            try
+            {
+                IDbCommand sqlCommand = new SqlCommand("SELECT everything FROM everywhere WHERE id=@id");
+                sqlCommand.Parameters.Add(new SqlParameter("@id", "1"));
+                sqlCommand.ExecuteReader();
+            }
+            catch
+            {
+                // sure it throws, what did you expect
+                // but command text should be logged to console
+            }
+        }
+    }
+}
---
+++ src/LogSqlAspects/LogAdoNetAttribute.cs
@@ -0,0 +1,58 @@
+﻿#region Using
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using log4net;
+using PostSharp.Extensibility;
+using PostSharp.Laos; 
+#endregion
+
+namespace LogSqlAspects
+{
+    [Serializable]
+    public class LogAdoNetAttribute : OnMethodInvocationAspect
+    {
+        #region Compile-time
+
+        public override void CompileTimeInitialize(MethodBase method)
+        {
+            MessageSource.MessageSink.Write(new Message(SeverityType.ImportantInfo, "LogAdoNetApplied", "Intercepted calls to " + method.DeclaringType.FullName + " " + method.Name, method.Name));
+        }
+
+        public override bool CompileTimeValidate(MethodBase method)
+        {
+            return method.DeclaringType.IsAssignableFrom(typeof(IDbCommand));
+        }
+
+        #endregion
+
+
+        #region Runtime
+
+        [NonSerialized]
+        private ILog _log;
+
+        public override void RuntimeInitialize(MethodBase method)
+        {
+            _log = LogManager.GetLogger(method.DeclaringType);
+        }
+
+        public override void OnInvocation(MethodInvocationEventArgs eventArgs)
+        {
+            if (_log.IsInfoEnabled)
+                LogCommand((IDbCommand)eventArgs.Instance);
+
+            eventArgs.Proceed();
+        }
+
+        private void LogCommand(IDbCommand command)
+        {
+            string paramString = String.Join(", ", command.Parameters.Cast<DbParameter>().Select(param => string.Format("{0}='{1}'", param.ParameterName, param.Value)).ToArray());
+            _log.Info(String.Format("Executing {0} {1}", command.CommandText, paramString));
+        }
+
+        #endregion
+    }
+}
